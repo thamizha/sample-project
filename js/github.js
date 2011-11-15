cbd2540f86f1386b30e06a731db86a252d0a2e77
Arunmozhi <aruntheguy@gmail.com>
2011-11-15 21:51:31 +0530

    added basic code to check

---
+++ js/github.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var project = $('#project').attr('name');
+    var baseurl = "https://api.github.com/";
+    var url = baseurl + "repos/thamizha/";
+    url += project;
+    url += "/collaborators";
+    $.get(url, function(data){
+            $('#collab').append(JSON.stringify(data));
+        });
+});
