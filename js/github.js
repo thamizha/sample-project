b751398ef06d7f119d0d21d4df5ddd3a2af0c711
Arunmozhi <aruntheguy@gmail.com>
2011-11-16 21:28:05 +0530

    simplified :P code

--- js/github.js
+++ js/github.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    var project = $('#project').attr('name');
+   // var project = $('#project').attr('name');
     var baseurl = "https://api.github.com/";
     var url = baseurl + "repos/thamizha/";
-    url += project;
+    url += "sample-project";
     url += "/collaborators";
-    $.get(url, function(data){
-            $('#collab').append(JSON.stringify(data));
+    $.getJSON(url, function(data){
+           // $('#collab').append(JSON.stringify(data));
 	    alert('Success');
         });
 });
